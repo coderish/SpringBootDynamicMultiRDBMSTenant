85b68f25d1af35e53eedd40d895351f4c7636698
coderish <32035238+coderish@users.noreply.github.com>
2021-03-17 19:20:15 +0530

    code commit

--- src/main/java/com/coderish/dynamic/multitenant/SpringBootDynamicMultiRDBMSTenantApplication.java
+++ src/main/java/com/coderish/dynamic/multitenant/SpringBootDynamicMultiRDBMSTenantApplication.java
@@ -8,12 +8,16 @@ public class SpringBootDynamicMultiRDBMSTenantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDynamicMultiRDBMSTenantApplication.class, args);
-		/*BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		/*
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String password = "password"; //ACV1254
 		String encodedPassword = passwordEncoder.encode(password);
 		System.out.println();
 		System.out.println("Password is         : " + password);
 		System.out.println("Encoded Password is : " + encodedPassword);
+
+//		Password is         : password
+//		Encoded Password is : $2a$10$/HTaU.jH42341zRymFzyFeECv2lgc4eSCbOvTZxii6lwov20xov.6
 
 		password = "diff_pass123"; // ACV1200
 		encodedPassword = passwordEncoder.encode(password);
@@ -21,11 +25,18 @@ public class SpringBootDynamicMultiRDBMSTenantApplication {
 		System.out.println("Password is         : " + password);
 		System.out.println("Encoded Password is : " + encodedPassword);
 
+//		Password is         : diff_pass123
+//		Encoded Password is : $2a$10$9yEhjj8AZ48XlKl71Bnk.eqlg.HX5/CS2i03x3AE9KBflhgj5HbGK
+
 		password = "postgres_pass123"; // ACV9999
 		encodedPassword = passwordEncoder.encode(password);
 		System.out.println();
 		System.out.println("Password is         : " + password);
-		System.out.println("Encoded Password is : " + encodedPassword);*/
+		System.out.println("Encoded Password is : " + encodedPassword);
+
+//		Password is         : postgres_pass123
+//		Encoded Password is : $2a$10$.hmvTcR4c/Syh7xgq7dj0.c23amrLHzXUAFzJZXCfgAteojWRiy52
+		*/
 	}
 
 }
